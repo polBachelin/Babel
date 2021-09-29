63726b5d7a7bac8f68a5851f7365162e3d66b9af
EvanSabre <evansabre@hotmail.fr>
2021-09-29 14:41:04 +0200

    feat(): merge with Network Server branch

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,41 +1,15 @@
 /*
 ** EPITECH PROJECT, 2021
-** babel
+** Babel
 ** File description:
 ** main
 */
 
-#include "TcpServer.hpp"
+#include "BabelClient.hpp"
 
-using asio::ip::tcp;
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  try
-  {
-    asio::io_context io_context;
+    Client::BabelClient client(argc, argv);
 
-    tcp::resolver resolver(io_context);
-    tcp::resolver::results_type endpoints =
-      resolver.resolve("127.0.0.1", "5000");
-
-    tcp::socket socket(io_context);
-    asio::connect(socket, endpoints);
-
-    packet_t orig;
-    packet_t *tmp = &orig;
-    tmp->magic = 562;
-    tmp->code = 3;
-    tmp->data_size = 256;
-    strcpy(tmp->data, "salut");
-    asio::error_code error;
-    std::size_t size = asio::write(socket, asio::buffer(tmp, sizeof(packet_t)), error);
-    std::cout << "message sent " << size << "  " << sizeof(packet_t) << std::endl;
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << e.what() << std::endl;
-  }
-
-  return 0;
+    return client.start();
 }
