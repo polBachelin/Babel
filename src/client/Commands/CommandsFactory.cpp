3304c13cd92ba8e485c671ecc5b8bb5b081b6950
simon.guyader@epitech.eu <simon.guyader@epitech.eu>
2021-10-13 22:08:45 +0200

    feat(CommandFactory): reworked command factory

--- src/client/Commands/CommandsFactory.cpp
+++ src/client/Commands/CommandsFactory.cpp
@@ -9,146 +9,64 @@
 
 using namespace Client;
 
+char *createBuffer(ClientInfos info, GUI::signal_e e, std::string data)
+{
+	packet_t package;
+	char *buffTemp = new char[sizeof(package)];
+
+	package.magic = MAGIC;
+	package.code = e;
+	package.data_size = data.size();
+	strcpy(package.data, data.c_str());
+
+	std::cout << "------------J'ai Envoyé----------------" << std::endl;
+	std::cout << "Magic = " << package.magic << std::endl;
+	std::cout << "Code  = " << package.code << std::endl;
+	std::cout << "size  = " << package.data_size << std::endl;
+	std::cout << "data  = " << package.data << std::endl;
+    std::cout << "---------------------------------------" << std::endl;
+
+	memcpy(buffTemp, &package, sizeof(package));
+	return buffTemp;
+}
+
 const std::unordered_map<GUI::signal_e,
 std::function<char *(ClientInfos, GUI::signal_e)>> CommandsFactory::_commands
 {
 	{GUI::signal_e::Elogin,
 	[](ClientInfos infos, GUI::signal_e e) {
 		std::cout << "Sending packet of login to server\n";
-		packet_t package;
-		char *buffTemp = new char[sizeof(package)];
-
-		package.magic = MAGIC;
-		package.code = e;
-		package.data_size = infos.username.size() + infos.password.size() + 2;
-		std::string dataStr(infos.username + "\n" + infos.password + "\n");
-		strcpy(package.data, dataStr.c_str());
-
-		std::cout << "------------ J'ai Envoyé ----------------" << std::endl;
-    	std::cout << "Magic = " << package.magic << std::endl;
-    	std::cout << "Code  = " << package.code << std::endl;
-    	std::cout << "size  = " << package.data_size << std::endl;
-    	std::cout << "data  = " << package.data << std::endl;
-    	std::cout << "--------------------------------------" << std::endl;
-
-		memcpy(buffTemp, &package, sizeof(package));
-		return buffTemp;
+		return createBuffer(infos, e, infos.username + "\n" + infos.password + "\n");
 	}},
 
 	{GUI::signal_e::Eregister,
 	[](ClientInfos infos, GUI::signal_e e) {
 		std::cout << "Sending packet of register to server\n";
-		packet_t package;
-		char *buffTemp = new char[sizeof(package)];
-
-		package.magic = MAGIC;
-		package.code = e;
-		package.data_size = infos.username.size() + infos.password.size() + 2;
-		std::string dataStr(infos.username + "\n" + infos.password + "\n");
-		strcpy(package.data, dataStr.c_str());
-
-		std::cout << "------------ J'ai Envoyé ----------------" << std::endl;
-    	std::cout << "Magic = " << package.magic << std::endl;
-    	std::cout << "Code  = " << package.code << std::endl;
-    	std::cout << "size  = " << package.data_size << std::endl;
-    	std::cout << "data  = " << package.data << std::endl;
-    	std::cout << "--------------------------------------" << std::endl;
-
-		memcpy(buffTemp, &package, sizeof(package));
-		return buffTemp;
+		return createBuffer(infos, e, infos.username + "\n" + infos.password + "\n");
 	}},
 
 	{GUI::signal_e::Eaddcontact,
 	[](ClientInfos infos, GUI::signal_e e) {
 		std::cout << "Sending packet of addContact to server\n";
-		packet_t package;
-		char *buffTemp = new char[sizeof(package)];
-
-		package.magic = MAGIC;
-		package.code = e;
-		package.data_size = infos.username.size() + 1;
-		std::string dataStr(infos.username + "\n");
-		strcpy(package.data, dataStr.c_str());
-
-		std::cout << "------------ J'ai Envoyé ----------------" << std::endl;
-    	std::cout << "Magic = " << package.magic << std::endl;
-    	std::cout << "Code  = " << package.code << std::endl;
-    	std::cout << "size  = " << package.data_size << std::endl;
-    	std::cout << "data  = " << package.data << std::endl;
-    	std::cout << "--------------------------------------" << std::endl;
-
-		memcpy(buffTemp, &package, sizeof(package));
-		return buffTemp;
+		return createBuffer(infos, e, infos.username + "\n");
 	}},
 
 	{GUI::signal_e::Erefuseincomingcall,
 	[](ClientInfos infos, GUI::signal_e e) {
 		std::cout << "Sending packet of refuse incoming call to server\n";
-		packet_t package;
-		char *buffTemp = new char[sizeof(package)];
-
-		package.magic = MAGIC;
-		package.code = e;
-		package.data_size = infos.username.size() + 1;
-		std::string dataStr(infos.username + "\n");
-		strcpy(package.data, dataStr.c_str());
-
-		std::cout << "------------ J'ai Envoyé ----------------" << std::endl;
-    	std::cout << "Magic = " << package.magic << std::endl;
-    	std::cout << "Code  = " << package.code << std::endl;
-    	std::cout << "size  = " << package.data_size << std::endl;
-    	std::cout << "data  = " << package.data << std::endl;
-    	std::cout << "--------------------------------------" << std::endl;
-
-		memcpy(buffTemp, &package, sizeof(package));
-		return buffTemp;
+		return createBuffer(infos, e, infos.username + "\n");
 	}},
 
 	{GUI::signal_e::EcallX,
 	[] (ClientInfos infos, GUI::signal_e e) {
 		std::cout << "Telling server to call X\n";
-		packet_t package;
-		char *buffTemp = new char[sizeof(package)];
-
-		package.magic = MAGIC;
-		package.code = e;
-		std::cout << infos.ip << std::endl;
-		package.data_size = infos.userToCall.size() + infos.ip.size() + infos.port.size() + 3;
-		std::string dataStr(infos.userToCall + "\n" + infos.ip + "\n" + infos.port + "\n");
-		strcpy(package.data, dataStr.c_str());
-
-		std::cout << "------------ J'ai Envoyé ----------------" << std::endl;
-    	std::cout << "Magic = " << package.magic << std::endl;
-    	std::cout << "Code  = " << package.code << std::endl;
-    	std::cout << "size  = " << package.data_size << std::endl;
-    	std::cout << "data  = " << package.data << std::endl;
-    	std::cout << "--------------------------------------" << std::endl;
-
-		memcpy(buffTemp, &package, sizeof(package));
-		return buffTemp;
+		return createBuffer(infos, e, infos.userToCall + "\n" + infos.ip + "\n6000\n");
 	}},
 
 	{GUI::signal_e::Easkcontactlist,
 	[](ClientInfos infos, GUI::signal_e e) {
 		std::cout << "Sending packet of ask contact list to server\n";
-		packet_t package;
-		char *buffTemp = new char[sizeof(package)];
-
-		package.magic = MAGIC;
-		package.code = e;
-		package.data_size = infos.username.size() + 1;
-		std::string dataStr(infos.username + "\n");
-		strcpy(package.data, dataStr.c_str());
-
-		std::cout << "------------ J'ai Envoyé ----------------" << std::endl;
-    	std::cout << "Magic = " << package.magic << std::endl;
-    	std::cout << "Code  = " << package.code << std::endl;
-    	std::cout << "size  = " << package.data_size << std::endl;
-    	std::cout << "data  = " << package.data << std::endl;
-    	std::cout << "--------------------------------------" << std::endl;
-
-		memcpy(buffTemp, &package, sizeof(package));
-		return buffTemp;
+		return createBuffer(infos, e, infos.username + "\n");
 	}}
 };
 
