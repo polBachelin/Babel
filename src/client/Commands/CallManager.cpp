1b0a538db1a1a06f7c747123d82ebe2c8a619a92
Pol Bachelin <pol.bachelin@epitech.eu>
2021-10-17 00:51:45 +0200

    fix(): fixed sizeof()

--- src/client/Commands/CallManager.cpp
+++ src/client/Commands/CallManager.cpp
@@ -126,7 +126,7 @@ void CallManager::onReadAudioData()
         // std::cout << "Network BuffSize : " << buffSize << std::endl;
         // std::cout << "---------------------------\n";
         compressed = new unsigned char[buffSize];
-        std::memcpy(compressed, (ptr + sizeof(std::time_t) + sizeof(buffSize)), buffSize * sizeof(*compressed));
+        std::memcpy(compressed, (ptr + sizeof(std::time_t) + sizeof(buffSize)), buffSize * sizeof(unsigned char));
         auto outputBuffer = new float[_inputBufferSize];
         _encoderManager->decode(compressed, outputBuffer, 480, buffSize);
         double max = 0;
